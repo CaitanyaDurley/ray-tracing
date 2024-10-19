a761747a9ffb4eb95768c0f7ecbf2df131773472
Caitanya Durley <caitanya.v.durley@gmail.com>
2024-10-19 14:18:31 +0100

    demo gradient image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use ray_tracing::{Image, Pixel};
+use ray_tracing::image_formatters::PPMFormatter;
+
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let side = 1024;
+    let gradient = |r, c| Pixel::new((255 * c / side) as u8, (255 * r / side) as u8, 0);
+    let blue = Image::new(side, side, gradient);
+    let mut ppm_formatter = PPMFormatter {};
+    blue.write_to_file(Path::new("tmp.ppm"), &mut ppm_formatter).unwrap();
 }
