ad4e55d7fea402270bdb0479b0e7d0028e3d666e
Caitanya Durley <caitanya.v.durley@gmail.com>
2024-10-19 18:53:08 +0100

    add support for binary PPM

--- src/image_formatters/ppm.rs
+++ src/image_formatters/ppm.rs
@@ -2,18 +2,34 @@ use crate::image::*;
 
 use std::iter;
 
-pub struct PPMFormatter {}
+pub struct PPMFormatter {
+    ascii_mode: bool,
+}
+
+impl PPMFormatter {
+    pub fn new(ascii_mode: bool) -> Self {
+        Self {
+            ascii_mode,
+        }
+    }
+}
 
 impl ImageFormatter for PPMFormatter {
     fn get_bytes(&mut self, image: &Image) -> impl Iterator<Item = Vec<u8>> {
+        let magic_number = if self.ascii_mode {
+            "P3"
+        } else {
+            "P6"
+        };
         Iterator::chain(
-            iter::once(format!("P3\n{} {}\n255\n", image.width, image.height).into_bytes()),
-            PPMIterator::new(image.height, image.width, &image.pixels)
+            iter::once(format!("{}\n{} {}\n255\n", magic_number, image.width, image.height).into_bytes()),
+            PPMIterator::new(self.ascii_mode, image.height, image.width, &image.pixels)
         )
     }
 }
 
 struct PPMIterator<'a> {
+    ascii_mode: bool,
     rows: u32,
     cols: u32,
     pixels: &'a[Pixel],
@@ -21,8 +37,9 @@ struct PPMIterator<'a> {
 }
 
 impl<'a> PPMIterator<'a> {
-    fn new(rows: u32, cols: u32, pixels: &'a[Pixel]) -> Self {
+    fn new(ascii_mode: bool, rows: u32, cols: u32, pixels: &'a[Pixel]) -> Self {
         Self {
+            ascii_mode,
             rows,
             cols,
             pixels,
@@ -41,6 +58,14 @@ impl<'a> Iterator for PPMIterator<'a> {
         }
         let pixel = self.pixels[self.ix as usize];
         self.ix += 1;
-        return Some(format!("{} {} {}\n", pixel.red, pixel.green, pixel.blue).into_bytes())
+        let mut out = vec![pixel.red, pixel.green, pixel.blue];
+        if self.ascii_mode {
+            out = out.iter().map(ToString::to_string)
+                .chain(iter::once("\n".to_string()))
+                .collect::<Vec<String>>()
+                .join(" ")
+                .into_bytes();
+        }
+        return Some(out)
     }
 }
